1c55f7993180ef3abcce7fd117c2de1b4490e991
Hchaichi Akrem <hchaichi-akrem@outlook.fr>
2020-09-17 17:14:27 +0100

    .

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const grapgQlSchema = require('./graphql/schema/index')
 const grapghQlResolvers = require('./graphql/resolvers/index')
 const isAuth = require('./middleware/is-auth')
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
 
 app.use(isAuth)
 app.use(boyParser.json())
