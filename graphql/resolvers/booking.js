46610ccc1a571f6638ef2e4c19ed0342009bca2b
Hchaichi Akrem <hchaichi-akrem@outlook.fr>
2020-09-19 21:46:28 +0100

    .

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -11,7 +11,7 @@ module.exports = {
             throw new Error('unauthenticated')
         }
         try {
-            const bookings = await Booking.find()
+            const bookings = await Booking.find({ user: req.userId })
             return bookings.map(booking => {
                 return tranforBooking(booking)
             })
--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,36 +1,44 @@
 const User = require('../../models/user')
 const Event = require('../../models/event')
 const { dateToString } = require('../../helpers/date')
+const DataLoader = require('dataloader');
 
+const eventLoader = new DataLoader((eventIds) => {
+    return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+    return User.find({ _id: { $in: userIds } });
+});
 
 const user = async userId => {
     try {
-        const user = await User.findById(userId)
+        const user = await userLoader.load(userId.toString());
         return {
             ...user._doc,
             _id: user.id,
-            createdEvents: events.bind(this, user._doc.createdEvents)
-        }
-    } catch (error) {
-        throw error
+            createdEvents: () => eventLoader.loadMany(user._doc.createdEvents)
+        };
+    } catch (err) {
+        throw err;
     }
-}
+};
 
 const events = async eventIds => {
     try {
-        const events = await Event.find({ _id: { $in: eventIds } })
+        const events = await Event.find({ _id: { $in: eventIds } });
         return events.map(event => {
-            return transformEvent(event)
-        })
-    } catch (error) {
-        throw error
+            return transformEvent(event);
+        });
+    } catch (err) {
+        throw err;
     }
-}
+};
 
 const singleEvent = async eventId => {
     try {
-        const event = await Event.findById(eventId)
-        return transformEvent(event)
+        const event = await eventLoader.load(eventId.toString());
+        return event
     } catch (error) {
         throw error
     }
@@ -52,7 +60,7 @@ const transformEvent = event => {
         ...event._doc,
         _id: event.id,
         date: dateToString(event._doc.date),
-        creator: user.bind(this, event._doc.creator)
+        creator: user.bind(this, event.creator)
     }
 }
 
